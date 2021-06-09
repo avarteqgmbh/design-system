c4c35d085e4f1353dd712866adfe6e40e0019d60
Sven Reiser <sreiser@anynines.com>
2021-06-09 12:51:31 +0200

    adds lint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>anynines Design System 2.0</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The next level anynines Design System providing a new Storybook package powered by Material UI.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
