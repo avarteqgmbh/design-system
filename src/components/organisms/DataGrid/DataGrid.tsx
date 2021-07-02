19b5cda897c8c72d30dce386ed3f5d885448ebe2
Sven Reiser <sreiser@anynines.com>
2021-07-02 10:41:07 +0200

    adds TextField and its Stories

--- src/components/organisms/DataGrid/DataGrid.tsx
+++ src/components/organisms/DataGrid/DataGrid.tsx
@@ -29,34 +29,6 @@ export function DataGrid(props: DataGridProps) {
 
 const useStyles = makeStyles<Theme>((theme) => ({
   root: {
-    background: theme.palette.background.paper,
-    border: 'none',
     
-    '& .MuiDataGrid-main': {
-      borderColor: theme.palette.background.border,
-      borderWidth: 1,
-      borderStyle: 'solid',
-      borderRadius: theme.radius.card,
-    },
-
-    '& .MuiDataGrid-toolbarContainer': {
-      padding: theme.spacing(3, 0),
-      '& .MuiButton-textPrimary': {
-        background: theme.palette.background.light,
-        color: theme.palette.text.secondary,
-        borderColor: theme.palette.background.border,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        marginRight: theme.spacing(3),
-        '& .MuiSvgIcon-root': {
-          maxHeight: 18,
-          maxWidth: 18,
-        },
-        '&:hover': {
-          background: theme.palette.background.medium,
-          color: theme.palette.text.primary,
-        }
-      }
-    },
   }
 }))
