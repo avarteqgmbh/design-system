e652c588b4b5cc049beda4c473fe24e6eac10692
sabbir0179 <sahmed@anynines.com>
2021-07-08 15:53:45 +0200

    Update ESLint and Prettier configuration and fix linter issues

--- src/components/inputs/Button/Button.tsx
+++ src/components/inputs/Button/Button.tsx
@@ -1,13 +1,11 @@
-import {
-  Button as MuiButton,
-  makeStyles,
-  ButtonProps,
-} from '@material-ui/core';
-import { Theme } from '../../../theme/types';
+import React from 'react'
+import { Button as MuiButton, makeStyles, ButtonProps } from '@material-ui/core'
 
-export function Button(props: ButtonProps) {
-  const { variant = 'contained', color = 'primary' } = props;
-  const classes = useStyles();
+import { Theme } from '../../../theme/types'
+
+export function Button(props: ButtonProps): JSX.Element {
+  const { variant = 'contained', color = 'primary' } = props
+  const classes = useStyles()
   return (
     <MuiButton
       classes={{
@@ -24,97 +22,99 @@ export function Button(props: ButtonProps) {
         containedSecondary: classes.containedSecondary,
         sizeSmall: classes.sizeSmall,
         sizeLarge: classes.sizeLarge,
-        disabled: classes.disabled,
+        disabled: classes.disabled
       }}
       variant={variant}
       color={color}
       {...props}
     />
-  );
+  )
 }
 
-const useStyles = makeStyles<Theme>((theme) => ({
-  root: {
-    display: 'flex',
-    padding: theme.spacing(3, 5),
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: theme.palette.background.border,
-    borderRadius: theme.radius.button,
-    background: theme.palette.background.paper,
-    boxShadow: 'none',
-    '&:hover': {
-      background: theme.shadows[0],
-    },
-  },
-  label: {
-    textTransform: 'none',
-    letterSpacing: 0,
-  },
-  text: {
-    borderColor: 'transparent',
-    background: 'transparent',
-    '&:hover': {
-      background: theme.palette.background.light,
-    }
-  },
-  textPrimary: {
-    borderColor: 'transparent',
-    background: 'transparent',
-    '&:hover': {
-      color: theme.palette.common.white,
-      background: theme.palette.primary.main,
-    }
-  },
-  sizeLarge: {
-    padding: theme.spacing(4, 6),
-  },
-  sizeSmall: {
-    padding: theme.spacing(2, 4),
-  },
-  outlined: {
-    borderColor: theme.palette.text.primary,
-    color: theme.palette.text.primary,
-    '&:hover': {
-      background: theme.palette.background.light,
-      borderColor: theme.palette.text.primary,
-    }
-  },
-  outlinedPrimary: {
-    borderColor: theme.palette.primary.main,
-    color: theme.palette.primary.main,
-    '&:hover': {
-      background: theme.palette.primary.main,
-      color: theme.palette.background.paper,
-      borderColor: theme.palette.primary.dark,
-    }
-  },
-  contained: {
-    background: theme.palette.background.paper,
-    color: theme.palette.text.primary,
-    '&:hover': {
-      background: theme.palette.background.light,
-    },
-  },
-  containedPrimary: {
-    background: theme.palette.primary.main,
-    color: theme.palette.common.white,
-    borderColor: theme.palette.primary.light,
-    '&:hover': {
-      background: theme.palette.primary.dark,
-    },
-  },
-  containedSecondary: {
-    background: theme.palette.background.light,
-    '&:hover': {
-      background: theme.palette.background.medium,
-    },
-  },
-  disabled: {
-    '&$disabled': {
-      color: theme.palette.text.disabled,
-      background: theme.palette.background.default,
+const useStyles = makeStyles<Theme>((theme) => {
+  return {
+    root: {
+      display: 'flex',
+      padding: theme.spacing(3, 5),
+      borderWidth: 1,
+      borderStyle: 'solid',
       borderColor: theme.palette.background.border,
+      borderRadius: theme.radius.button,
+      background: theme.palette.background.paper,
+      boxShadow: 'none',
+      '&:hover': {
+        background: theme.shadows[0]
+      }
     },
-  },
-}));
+    label: {
+      textTransform: 'none',
+      letterSpacing: 0
+    },
+    text: {
+      borderColor: 'transparent',
+      background: 'transparent',
+      '&:hover': {
+        background: theme.palette.background.light
+      }
+    },
+    textPrimary: {
+      borderColor: 'transparent',
+      background: 'transparent',
+      '&:hover': {
+        color: theme.palette.common.white,
+        background: theme.palette.primary.main
+      }
+    },
+    sizeLarge: {
+      padding: theme.spacing(4, 6)
+    },
+    sizeSmall: {
+      padding: theme.spacing(2, 4)
+    },
+    outlined: {
+      borderColor: theme.palette.text.primary,
+      color: theme.palette.text.primary,
+      '&:hover': {
+        background: theme.palette.background.light,
+        borderColor: theme.palette.text.primary
+      }
+    },
+    outlinedPrimary: {
+      borderColor: theme.palette.primary.main,
+      color: theme.palette.primary.main,
+      '&:hover': {
+        background: theme.palette.primary.main,
+        color: theme.palette.background.paper,
+        borderColor: theme.palette.primary.dark
+      }
+    },
+    contained: {
+      background: theme.palette.background.paper,
+      color: theme.palette.text.primary,
+      '&:hover': {
+        background: theme.palette.background.light
+      }
+    },
+    containedPrimary: {
+      background: theme.palette.primary.main,
+      color: theme.palette.common.white,
+      borderColor: theme.palette.primary.light,
+      '&:hover': {
+        background: theme.palette.primary.dark
+      }
+    },
+    containedSecondary: {
+      background: theme.palette.background.light,
+      '&:hover': {
+        background: theme.palette.background.medium
+      }
+    },
+    disabled: {
+      '&$disabled': {
+        color: theme.palette.text.disabled,
+        background: theme.palette.background.default,
+        borderColor: theme.palette.background.border
+      }
+    }
+  }
+})
--- src/theme/types.ts
+++ src/theme/types.ts
@@ -1,54 +1,54 @@
 import {
   Color,
   Theme as MuiTheme,
-  ThemeOptions as MuiThemeOptions,
-} from '@material-ui/core';
+  ThemeOptions as MuiThemeOptions
+} from '@material-ui/core'
 import {
   Palette as MuiPalette,
   PaletteColor,
-  TypeBackground,
-} from '@material-ui/core/styles/createPalette';
+  TypeBackground
+} from '@material-ui/core/styles/createPalette'
 
 export interface Background extends TypeBackground {
-  light?: string;
-  medium?: string;
-  border?: string;
+  light?: string
+  medium?: string
+  border?: string
 }
 export interface Grey extends Color {
-  1000?: string;
+  1000?: string
 }
 
 export interface Gradients {
-  daylight?: string;
-  midnight?: string;
-  primary?: string;
+  daylight?: string
+  midnight?: string
+  primary?: string
 }
 export interface Primary extends PaletteColor {
-  lighter?: string;
-  darker?: string;
+  lighter?: string
+  darker?: string
 }
 
 export interface Palette extends MuiPalette {
-  background: Background;
-  grey: Grey;
-  gradients: Gradients;
-  primary: Primary;
+  background: Background
+  grey: Grey
+  gradients: Gradients
+  primary: Primary
 }
 
 export interface Radius {
-  none?: number;
-  small?: number;
-  medium?: number;
-  large?: number;
-  button?: number;
-  card?: number;
+  none?: number
+  small?: number
+  medium?: number
+  large?: number
+  button?: number
+  card?: number
 }
 
 export interface Theme extends MuiTheme {
-  palette: Palette;
-  radius: Radius;
+  palette: Palette
+  radius: Radius
 }
 export interface ThemeOptions extends MuiThemeOptions {
-  palette: Palette;
-  radius: Radius;
+  palette: Palette
+  radius: Radius
 }
