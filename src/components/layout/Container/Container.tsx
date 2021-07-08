1649df81b28dacd5822a8792ccc4a25bed113d1a
Momen-a9s <82593085+Momen-a9s@users.noreply.github.com>
2021-07-08 10:14:59 +0200

    Adding Mui Container Component (#7)
    * Adding Mui Container Component

---
+++ src/components/layout/Container/Container.tsx
@@ -0,0 +1,22 @@
+import {
+  Container as MuiContainer,
+  ContainerProps as MuiContainerProps,
+  makeStyles,
+} from '@material-ui/core';
+import { Theme } from '../../../theme/types';
+
+export function Container(props: MuiContainerProps) {
+  const classes = useStyles();
+  return (
+    <MuiContainer
+      className={`${classes.root}`}
+      {...props}
+    />
+  );
+}
+
+const useStyles = makeStyles<Theme>((theme) => ({
+  root: {    
+  },
+}));
+  
