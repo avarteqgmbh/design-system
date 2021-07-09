4090e01d591fa8731d7e7ce9ada60b9d707f4a5f
Momen Yacoub <myacoub@anynines.com>
2021-07-09 08:48:20 +0200

    adding Grid Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,9 +4,11 @@ import { DataGrid } from './labs/index'
 
 import { Link, MenuItem } from './navigation/index'
 
-import { Box, Container } from './layout/index'
+import { Box, Container, Grid } from './layout/index'
+import { Paper } from './surfaces/index'
 
 export { Button, Checkbox, Select, TextField }
 export { DataGrid }
 export { Link, MenuItem }
-export { Box, Container }
+export { Box, Container, Grid }
+export { Paper }
