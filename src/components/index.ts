5d1867b876b723db5d101818870bad2d5d0bd345
Momen Yacoub <myacoub@anynines.com>
2021-07-13 10:02:45 +0200

    adding Hidden Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,11 +10,11 @@ import { DataGrid } from './labs/index'
 
 import { Link, MenuItem } from './navigation/index'
 
-import { Box, Container, Grid } from './layout/index'
+import { Box, Container, Grid, Hidden } from './layout/index'
 import { Paper } from './surfaces/index'
 
 export { Button, ButtonGroup, Checkbox, Select, TextField }
 export { DataGrid }
 export { Link, MenuItem }
-export { Box, Container, Grid }
+export { Box, Container, Grid, Hidden }
 export { Paper }
