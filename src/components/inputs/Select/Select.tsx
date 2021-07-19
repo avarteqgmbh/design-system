0a4eea9c269937260b18a2cfbdcf7f80690631a4
Momen-a9s <82593085+Momen-a9s@users.noreply.github.com>
2021-07-19 03:43:12 +0200

    adding more examples for select component (#20)

--- src/components/inputs/Select/Select.tsx
+++ src/components/inputs/Select/Select.tsx
@@ -1,6 +1,27 @@
 import React from 'react'
-import { Select as MuiSelect, SelectProps } from '@material-ui/core'
+import {
+  Select as MuiSelect,
+  SelectProps as MuiSelectProps,
+  makeStyles
+} from '@material-ui/core'
+import { Theme } from '../../../theme/types'
 
-export function Select(props: SelectProps): JSX.Element {
-  return <MuiSelect {...props} />
+export function Select(props: MuiSelectProps): JSX.Element {
+  const classes = useStyles()
+  const { variant = 'outlined' } = props
+  return (
+    <MuiSelect
+      classes={{
+        root: classes.root
+      }}
+      variant={variant}
+      {...props}
+    />
+  )
 }
+
+const useStyles = makeStyles<Theme>(() => {
+  return {
+    root: {}
+  }
+})
