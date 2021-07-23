3542127fb95f761da8dd6fb0a61e92ab085154fe
sabbir0179 <sahmed@anynines.com>
2021-07-23 18:56:09 +0200

    Add persistent drawer and improve imports on Navigation

--- src/components/navigation/Drawer/Drawer.stories.tsx
+++ src/components/navigation/Drawer/Drawer.stories.tsx
@@ -4,11 +4,28 @@ import { withDesign } from 'storybook-addon-designs'
 
 import { Drawer } from './Drawer'
 import { Button } from '../../inputs/Button/Button'
+import { List } from '../../dataDisplay/List/List'
 
 import clsx from 'clsx'
-import { makeStyles } from '@material-ui/core/styles'
-import { List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core'
-import { Mail as MailIcon, Inbox as InboxIcon } from '@material-ui/icons'
+import { makeStyles, createStyles, useTheme } from '@material-ui/core/styles'
+import {
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  Typography,
+  Divider,
+  IconButton
+} from '@material-ui/core'
+import {
+  Mail as MailIcon,
+  Inbox as InboxIcon,
+  ChevronLeft as ChevronLeftIcon,
+  ChevronRight as ChevronRightIcon,
+  Menu as MenuIcon
+} from '@material-ui/icons'
 
 export default {
   title: 'Navigation/Drawer',
@@ -23,7 +40,7 @@ export default {
   }
 }
 
-const useStyles = makeStyles({
+const useDefaultStyles = makeStyles({
   list: {
     width: 250
   },
@@ -35,7 +52,7 @@ const useStyles = makeStyles({
 type Anchor = 'top' | 'left' | 'bottom' | 'right' | undefined
 
 export const Default: Story<DrawerProps> = (): JSX.Element => {
-  const classes = useStyles()
+  const classes = useDefaultStyles()
   const [state, setState] = React.useState({
     top: false,
     left: false,
@@ -103,6 +120,147 @@ export const Default: Story<DrawerProps> = (): JSX.Element => {
           )
         }
       )}
+    </div>
+  )
+}
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    root: {
+      display: 'flex'
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    hide: {
+      display: 'none'
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end'
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      marginLeft: -drawerWidth
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      }),
+      marginLeft: 0
+    }
+  })
+})
+
+export const PersistentDrawer = (): JSX.Element => {
+  const classes = useStyles()
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
+
+  const handleDrawerOpen = (): void => {
+    setOpen(true)
+  }
+
+  const handleDrawerClose = (): void => {
+    setOpen(false)
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position='fixed'
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap>
+            Persistent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant='persistent'
+        anchor='left'
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => {
+            return (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          })}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
     </div>
   )
 }
