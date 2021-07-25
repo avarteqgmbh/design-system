96a4d74e8451e84c76a679112cd1b6d27754dae1
sabbir0179 <sahmed@anynines.com>
2021-07-25 22:14:06 +0200

    Add Divider mui story

--- src/components/dataDisplay/Divider/Divider.stories.tsx
+++ src/components/dataDisplay/Divider/Divider.stories.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Story } from '@storybook/react'
 import { withDesign } from 'storybook-addon-designs'
-import { ListItem, ListItemText, DividerProps } from '@material-ui/core'
+import {
+  ListItem,
+  ListItemText,
+  DividerProps,
+  ListItemAvatar,
+  Avatar
+} from '@material-ui/core'
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles'
+import {
+  Work as WorkIcon,
+  BeachAccess as BeachAccessIcon,
+  Image as ImageIcon
+} from '@material-ui/icons'
 
 import { Divider } from './Divider'
 import { List } from '../List/List'
+import { Grid } from '../../layout/Grid/Grid'
 
 export default {
   title: 'Data Display/Divider',
@@ -50,5 +63,73 @@ export const Default: Story<DividerProps> = (args): JSX.Element => {
         <ListItemText primary='Spam' />
       </ListItem>
     </List>
+  )
+}
+
+export const Inset: Story<DividerProps> = (args): JSX.Element => {
+  const classes = useStyles()
+
+  return (
+    <List className={classes.root}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary='Photos' secondary='Jan 9, 2014' />
+      </ListItem>
+      <Divider variant='inset' component='li' {...args} />
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary='Work' secondary='Jan 7, 2014' />
+      </ListItem>
+      <Divider variant='inset' component='li' {...args} />
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary='Vacation' secondary='July 20, 2014' />
+      </ListItem>
+    </List>
+  )
+}
+
+const useVerticalStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    root: {
+      width: 'fit-content',
+      border: `1px solid ${theme.palette.divider}`,
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: theme.palette.background.paper,
+      color: theme.palette.text.secondary,
+      '& svg': {
+        margin: theme.spacing(1.5)
+      },
+      '& hr': {
+        margin: theme.spacing(0, 0.5)
+      }
+    }
+  })
+})
+
+export const Vertical: Story<DividerProps> = (): JSX.Element => {
+  const classes = useVerticalStyles()
+
+  return (
+    <div>
+      <Grid container alignItems='center' className={classes.root}>
+        <BeachAccessIcon />
+        <Divider orientation='vertical' flexItem />
+        <WorkIcon />
+        <ImageIcon />
+      </Grid>
+    </div>
   )
 }
