29f6bee07737b71e3e72b4326da0ee0a2aa4cd1c
Momen Yacoub <myacoub@anynines.com>
2021-07-26 10:17:25 +0200

    adding typography component

--- src/components/dataDisplay/index.ts
+++ src/components/dataDisplay/index.ts
@@ -1,4 +1,5 @@
 import { Chip } from './Chip/Chip'
 import { List } from './List/List'
+import { Typography } from './Typography/Typography'
 
-export { Chip, List }
+export { Chip, List, Typography }
