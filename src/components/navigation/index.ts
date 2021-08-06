485131449e73190d71a314a18190ad7490e56bf0
sabbir0179 <sahmed@anynines.com>
2021-08-06 17:55:28 +0200

    Add mui components for Stepper and examples in its story

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -5,10 +5,15 @@ import { Breadcrumbs } from './Breadcrumbs/Breadcrumbs'
 import { BottomNavigation } from './BottomNavigation/BottomNavigation'
 import { BottomNavigationAction } from './BottomNavigation/BottomNavigationAction'
 import { Drawer } from './Drawer/Drawer'
-
 import { Tabs } from './Tabs/Tabs'
 import { Tab } from './Tabs/Tab'
 import { TabPanel } from './Tabs/TabPanel'
+import { Step } from './Stepper/Step'
+import { Stepper } from './Stepper/Stepper'
+import { MobileStepper } from './Stepper/MobileStepper'
+import { StepLabel } from './Stepper/StepLabel'
+import { StepButton } from './Stepper/StepButton'
+import { StepContent } from './Stepper/StepContent'
 
 export {
   Link,
@@ -20,5 +25,11 @@ export {
   Drawer,
   Tabs,
   Tab,
-  TabPanel
+  TabPanel,
+  Step,
+  Stepper,
+  StepLabel,
+  StepButton,
+  StepContent,
+  MobileStepper
 }
