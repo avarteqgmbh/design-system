58b8c0f653e0a29707aa05df0d5315feb28db13d
Niklas Kiefer <nkiefer@anynines.com>
2021-12-17 16:03:14 +0100

    Update: DateRange Component, add props, adjust test-story CHAP-1699

--- src/components/Test.stories.tsx
+++ src/components/Test.stories.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Story } from '@storybook/react'
 import { withDesign } from 'storybook-addon-designs'
 import { DateRangePicker } from './inputs/Pickers/DateRange'
+import TextField from '@mui/material/TextField'
+import { DateRange } from '@mui/lab/DateRangePicker'
+import DateAdapter from '@mui/lab/AdapterDayjs'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import Box from '@mui/material/Box'
 
 export default {
   title: 'Test/Playground',
@@ -16,8 +21,30 @@ export default {
   }
 }
 
-const Template: Story = (args) => {
-  return <DateRangePicker {...args} />
+const Template: Story = () => {
+  const [value, setValue] = React.useState<DateRange<Date>>([null, null])
+
+  return (
+    <LocalizationProvider dateAdapter={DateAdapter}>
+      <DateRangePicker
+        startText='Check-in'
+        endText='Check-out'
+        value={value}
+        onChange={(newValue): void => {
+          return setValue(newValue as DateRange<Date>)
+        }}
+        renderInput={(startProps, endProps): JSX.Element => {
+          return (
+            <>
+              <TextField {...startProps} />
+              <Box sx={{ mx: 2 }}> to </Box>
+              <TextField {...endProps} />
+            </>
+          )
+        }}
+      />
+    </LocalizationProvider>
+  )
 }
 
 export const Default = Template.bind({})
--- src/components/inputs/Pickers/DateRange.tsx
+++ src/components/inputs/Pickers/DateRange.tsx
@@ -1,43 +1,19 @@
 import * as React from 'react'
-import TextField from '@mui/material/TextField'
-import StaticDateRangePicker from '@mui/lab/StaticDateRangePicker'
-import DateAdapter from '@mui/lab/AdapterDayjs'
-import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import MuiDateRangePicker, {
+  DateRangePickerProps
+} from '@mui/lab/DateRangePicker'
 import { LicenseInfo } from '@mui/x-data-grid-pro'
-import Box from '@mui/material/Box'
-import { DateRange } from '@mui/lab/DateRangePicker'
 import { makeStyles } from '../../../theme/ThemeProvider'
 import { Theme } from '../../../theme/types'
 
-export function DateRangePicker(): JSX.Element {
-  const [value, setValue] = React.useState<DateRange<Date>>([null, null])
+export function DateRangePicker(
+  props: DateRangePickerProps<Date>
+): JSX.Element {
   const classes = useStyles()
 
   LicenseInfo.setLicenseKey(process.env.REACT_APP_MUI_LICENSE || '')
 
-  return (
-    <LocalizationProvider dateAdapter={DateAdapter}>
-      <StaticDateRangePicker
-        startText='Check-in'
-        endText='Check-out'
-        displayStaticWrapperAs='desktop'
-        value={value}
-        className={classes.root}
-        onChange={(newValue): void => {
-          return setValue(newValue as DateRange<Date>)
-        }}
-        renderInput={(startProps, endProps): JSX.Element => {
-          return (
-            <>
-              <TextField {...startProps} />
-              <Box sx={{ mx: 2 }}> to </Box>
-              <TextField {...endProps} />
-            </>
-          )
-        }}
-      />
-    </LocalizationProvider>
-  )
+  return <MuiDateRangePicker {...props} className={classes.root} />
 }
 
 const useStyles = makeStyles((theme: Theme) => {
