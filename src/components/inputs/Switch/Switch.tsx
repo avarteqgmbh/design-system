c6f7e0ea2f5dc837d7aa70640196305ea4d7c741
Niklas Kiefer <nkiefer@anynines.com>
2022-04-14 15:27:54 +0200

    Add: Inline Option to Switch CHAP-2024

--- src/components/inputs/Switch/Switch.tsx
+++ src/components/inputs/Switch/Switch.tsx
@@ -6,13 +6,20 @@ import {
 import makeStyles from '@mui/styles/makeStyles'
 import { Theme } from '../../../theme/types'
 
-export function Switch(props: MuiSwitchProps): JSX.Element {
+export interface SwitchProps extends MuiSwitchProps {
+  inline?: boolean | undefined
+}
+
+export function Switch(props: SwitchProps): JSX.Element {
   const classes = useStyles()
+  const { inline } = props
+
   return (
     <MuiSwitch
       classes={{
         root: classes.root
       }}
+      className={inline ? 'inline' : ''}
       {...props}
     />
   )
@@ -20,6 +27,27 @@ export function Switch(props: MuiSwitchProps): JSX.Element {
 
 const useStyles = makeStyles<Theme>(() => {
   return {
-    root: {}
+    root: {
+      '&.inline': {
+        padding: 8,
+        '& .MuiSwitch-track': {
+          borderRadius: 22 / 2,
+          '&:before, &:after': {
+            content: '""',
+            position: 'absolute',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            width: 16,
+            height: 16
+          }
+        },
+        '& .MuiSwitch-thumb': {
+          boxShadow: 'none',
+          width: 16,
+          height: 16,
+          margin: 2
+        }
+      }
+    }
   }
 })
