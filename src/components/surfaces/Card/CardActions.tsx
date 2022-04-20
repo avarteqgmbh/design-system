564e3e58d1097851a8d240957ff0f8bc1b21914b
Niklas Kiefer <nkiefer@anynines.com>
2022-04-20 15:20:20 +0200

    Update: Padding for Card Footer CHAP-2028

--- src/components/surfaces/Card/CardActions.tsx
+++ src/components/surfaces/Card/CardActions.tsx
@@ -3,5 +3,9 @@ import MuiCardActions, { CardActionsProps } from '@mui/material/CardActions'
 
 export function CardActions(props: CardActionsProps): JSX.Element {
   const { children } = props
-  return <MuiCardActions {...props}>{children}</MuiCardActions>
+  return (
+    <MuiCardActions sx={{ padding: '4px 16px 16px 16px' }} {...props}>
+      {children}
+    </MuiCardActions>
+  )
 }
