932a7f06fa1749cb187040b35a2da5cc50f9d2e7
Niklas Kiefer <nkiefer@anynines.com>
2022-04-21 10:01:02 +0200

    Update: Styling for XGrid and remove unneeded overrides CHAP-2029

--- src/components/labs/XGrid/XGrid.tsx
+++ src/components/labs/XGrid/XGrid.tsx
@@ -16,10 +16,8 @@ import {
   GridApiRef,
   LicenseInfo
 } from '@mui/x-data-grid-pro'
-import GlobalStyles from '@mui/material/GlobalStyles'
 import { GRID_DE_LOCALE_TEXT } from './locales'
 import { makeStyles } from '../../../theme/ThemeProvider'
-import { Theme } from '../../../theme/types'
 import { QuickSearch } from './QuickSearch'
 import {
   useLocalStorage,
@@ -58,11 +56,13 @@ export function XGrid(props: XGridProps): JSX.Element {
     setSearchText,
     toolbar = false
   } = props
+
   const classes = useStyles()
   const [tableConfig, setTableConfig] = useLocalStorage<object>(
     localStorageKey,
     {}
   )
+
   const CustomToolbar = (): JSX.Element => {
     return (
       <GridToolbarContainer>
@@ -112,7 +112,6 @@ export function XGrid(props: XGridProps): JSX.Element {
 
   return (
     <>
-      {GridGlobalStyles}
       <DataGridPro
         apiRef={customApiRef}
         localeText={lang}
@@ -126,67 +125,9 @@ export function XGrid(props: XGridProps): JSX.Element {
   )
 }
 
-const GridGlobalStyles = (
-  <GlobalStyles
-    styles={{
-      '& .MuiGridMenu-root .MuiPaper-root': {
-        background: 'white'
-      },
-      '& .MuiGridPanel-paper': {
-        '& .MuiSwitch-colorPrimary.Mui-checked + .MuiSwitch-track': {
-          backgroundColor: '#cdcdcd'
-        },
-        '& .MuiFormControl-root': {
-          '& .MuiFormLabel-root.Mui-focused': {
-            color: '#2e2e2e'
-          },
-          '& .MuiInput-underline:after': {
-            borderColor: '#2e2e2e'
-          }
-        },
-        '& .MuiButton-textPrimary': {
-          color: '#2e2e2e',
-          backgroundColor: 'white',
-          '&:hover': {
-            backgroundColor: '#efefef'
-          }
-        },
-        '& .MuiInputBase-input': {
-          backgroundColor: 'transparent'
-        },
-        '& .MuiSwitch-colorPrimary.Mui-checked': {
-          color: '#2e2e2e'
-        }
-      }
-    }}
-  />
-)
-
-const useStyles = makeStyles((theme: Theme) => {
+const useStyles = makeStyles(() => {
   return {
     root: {
-      borderColor: theme.palette.background.border,
-
-      '& *': {
-        fontFamily: theme.typography.fontFamily
-      },
-
-      '& .MuiDataGrid-columnHeader:focus, .MuiDataGrid-cell:focus': {
-        outline: `solid ${theme.palette.primary.main} 1px`
-      },
-
-      '& .MuiCheckbox-colorPrimary.Mui-checked': {
-        color: theme.palette.primary.main
-      },
-
-      '& .MuiDataGrid-row:hover': {
-        backgroundColor: `${theme.palette.background.default} !important`
-      },
-
-      '& .MuiDataGrid-row.Mui-selected': {
-        backgroundColor: `${theme.palette.background.light} !important`
-      },
-
       '& .MuiDataGrid-columnHeaderTitle, .MuiDataGrid-cell': {
         fontSize: 16
       },
@@ -195,20 +136,11 @@ const useStyles = makeStyles((theme: Theme) => {
         fontSize: 16
       },
 
-      '& .MuiBadge-colorPrimary': {
-        backgroundColor: theme.palette.primary.main
-      },
-
       '& .MuiDataGrid-toolbarContainer': {
-        backgroundColor: theme.palette.background.paper,
+        padding: 8,
 
-        '& .MuiButton-textPrimary': {
-          color: theme.palette.primary.main,
-          backgroundColor: theme.palette.background.paper,
-
-          '&:hover': {
-            backgroundColor: theme.palette.background.light
-          }
+        '& .MuiButton-root.MuiButton-text': {
+          marginRight: 8
         }
       }
     }
