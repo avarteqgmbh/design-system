3c5e78aae4d27dca71194094d246fae4fa7e45ad
Niklas Kiefer <nkiefer@anynines.com>
2022-04-22 12:46:33 +0200

    Remove: makeStyles from surfaces CHAP-2042

--- src/components/surfaces/Card/CardHeader.tsx
+++ src/components/surfaces/Card/CardHeader.tsx
@@ -3,5 +3,6 @@ import MuiCardHeader, { CardHeaderProps } from '@mui/material/CardHeader'
 
 export function CardHeader(props: CardHeaderProps): JSX.Element {
   const { children } = props
+
   return <MuiCardHeader {...props}>{children}</MuiCardHeader>
 }
