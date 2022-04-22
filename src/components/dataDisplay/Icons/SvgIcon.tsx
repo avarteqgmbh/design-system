3790abcf394ec3e96935071f63869654d335b055
Niklas Kiefer <nkiefer@anynines.com>
2022-04-22 15:28:41 +0200

    Remove: rest of makeStyles CHAP-2042

--- src/components/dataDisplay/Icons/SvgIcon.tsx
+++ src/components/dataDisplay/Icons/SvgIcon.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { SvgIcon as MuiSvgIcon, SvgIconProps } from '@mui/material'
 
 export function SvgIcon(props: SvgIconProps): JSX.Element {
-  const { children } = props
+  const { children, sx } = props
 
-  return <MuiSvgIcon {...props}>{children}</MuiSvgIcon>
+  return (
+    <MuiSvgIcon sx={{ ...sx, ...styles }} {...props}>
+      {children}
+    </MuiSvgIcon>
+  )
+}
+
+const styles = {
+  '&.MuiSvgIcon-colorDefault': {
+    color: 'text.primary'
+  }
 }
