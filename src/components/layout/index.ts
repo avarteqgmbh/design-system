9f409846e8f27fc3fed8af6fb39340f957edb9ba
Niklas Kiefer <nkiefer@anynines.com>
2022-04-27 12:54:45 +0200

    Version 1.0.4

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,7 +1,6 @@
 import { Box } from './Box/Box'
 import { Container } from './Container/Container'
 import { Grid, GridDirection } from './Grid/Grid'
-import { Hidden } from './Hidden/Hidden'
 
-export { Box, Container, Grid, Hidden }
+export { Box, Container, Grid }
 export type { GridDirection }
