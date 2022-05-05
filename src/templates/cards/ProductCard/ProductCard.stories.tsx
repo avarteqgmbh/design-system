404c39dcf6361c045a5116ca3332910ac3526317
Niklas Kiefer <nkiefer@anynines.com>
2022-05-05 14:57:42 +0200

    Update: ProductCard CHAP-2073

--- src/templates/cards/ProductCard/ProductCard.stories.tsx
+++ src/templates/cards/ProductCard/ProductCard.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Story } from '@storybook/react'
 
 import { withDesign } from 'storybook-addon-designs'
+import { Box } from '../../../index'
 import { ProductCard, ProductCardProps } from './ProductCard'
 
 export default {
@@ -14,7 +15,8 @@ export default {
       table: {
         type: { summary: 'func' }
       }
-    }
+    },
+    gridSize: { type: 'number', defaultValue: 4 }
   },
   parameters: {
     controls: { expanded: true },
@@ -25,15 +27,73 @@ export default {
     muiDocSrc: ''
   }
 }
+interface ProductCardStoryProps extends ProductCardProps {
+  gridSize: number
+}
 
-const Template: Story<ProductCardProps> = (args) => {
-  return <ProductCard {...args} />
+const Template: Story<ProductCardStoryProps> = (args) => {
+  const products = [
+    {
+      name: args.name,
+      points: args.points,
+      imageUrl: args.imageUrl,
+      tags: args.tags,
+      backgroundImage: args.backgroundImage,
+      hoverAnimation: args.hoverAnimation
+    },
+    {
+      name: 'Bart Öl "Hipster"',
+      points: 450,
+      imageUrl:
+        'https://images.pexels.com/photos/6693952/pexels-photo-6693952.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      tags: ['Nur noch 3 verfügbar'],
+      backgroundImage: true,
+      hoverAnimation: args.hoverAnimation
+    },
+    {
+      name: 'Seifenspender "Bjiörn Malte Matze"',
+      points: 790,
+      imageUrl:
+        'https://images.pexels.com/photos/5797999/pexels-photo-5797999.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+      tags: ['Auf Lager', 'Varianten verfügbar'],
+      backgroundImage: true,
+      hoverAnimation: args.hoverAnimation
+    },
+    {
+      name: 'Gaderobe "Hang In There"',
+      points: 3450,
+      imageUrl:
+        'https://images.pexels.com/photos/4210315/pexels-photo-4210315.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      tags: ['Nicht verfügbar'],
+      backgroundImage: true,
+      hoverAnimation: args.hoverAnimation
+    }
+  ]
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${args.gridSize}, 1fr)`,
+          gap: 6,
+          mb: 6
+        }}
+      >
+        {products &&
+          products.map((product) => {
+            return <ProductCard {...args} {...product} />
+          })}
+      </Box>
+    </Box>
+  )
 }
 
 export const Default = Template.bind({})
 Default.args = {
-  name: 'Title',
+  name: 'Fuji Kamera "Old School/ Vintage"',
   points: 1050,
-  image:
-    'https://images.pexels.com/photos/2468056/pexels-photo-2468056.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  imageUrl:
+    'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  tags: ['Auf Lager', 'Varianten verfügbar']
 }
--- src/templates/cards/ProductCard/ProductCard.tsx
+++ src/templates/cards/ProductCard/ProductCard.tsx
@@ -1,62 +1,128 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react'
-import { Box, Typography } from '../../../index'
-import Points from './Points'
+import TextTruncate from 'react-text-truncate'
+import { Theme } from '../../../theme/types'
+import { Box, Chip, Typography } from '../../../index'
+import Points from '../Points'
 
 export interface ProductCardProps {
   name: string
   points?: number
-  image?: string
+  backgroundImage?: boolean
+  imageUrl?: string
+  hoverAnimation?: boolean
+  novelty?: boolean
+  tags?: string[]
   onClick: () => void
 }
 
 export const ProductCard: React.FC<ProductCardProps> = (props) => {
-  const { name, points, onClick, image } = props
+  const {
+    name,
+    points,
+    backgroundImage = true,
+    imageUrl,
+    novelty,
+    hoverAnimation = true,
+    tags,
+    onClick
+  } = props
+
+  const classes = {
+    root: {
+      borderRadius: (theme: Theme): string => {
+        return `${theme.radius.card}px`
+      },
+      bgcolor: 'background.paper',
+      overflow: 'hidden',
+      transition: '200ms all ease-in-out',
+      boxShadow: 1,
+      cursor: 'pointer',
+      height: '100%',
+      minWidth: 260,
+      '&:hover': {
+        transform: hoverAnimation ? 'translateY(-4px)' : '',
+        '& .image': {
+          transform: hoverAnimation ? 'scale(1.05)' : ''
+        }
+      }
+    },
+    backgroundImageWrapper: {
+      position: 'relative',
+      width: '100%',
+      height: 180,
+      display: 'flex',
+      overflow: 'hidden'
+    },
+    imageWrapper: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'relative',
+      width: '100%',
+      height: 180,
+      '& img': {
+        maxHeight: 150
+      }
+    },
+    backgroundImage: {
+      transition: '200ms all ease-in-out',
+      backgroundImage: `url(${imageUrl})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height: '100%',
+      width: '100%'
+    },
+    noveltyLabel: {
+      position: 'absolute',
+      top: 16,
+      right: 16
+    },
+    tagsWrapper: {
+      display: 'flex',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      mb: 2
+    }
+  }
 
   return (
     <Box
       onClick={(): void => {
         return onClick()
       }}
-      sx={{
-        borderRadius: '8px',
-        height: '100%',
-        bgcolor: 'background.paper',
-        overflow: 'hidden',
-        transition: '200ms all ease-in-out',
-        boxShadow: 1,
-        cursor: 'pointer',
-        '&:hover': {
-          transform: 'translateY(-4px)',
-          '& .image': {
-            transform: 'scale(1.05)'
-          }
-        }
-      }}
+      // @ts-ignore
+      sx={classes.root}
     >
-      <Box
-        sx={{
-          width: '100%',
-          height: 180,
-          overflow: 'hidden'
-        }}
-      >
-        <Box
-          className='image'
-          sx={{
-            transition: '200ms all ease-in-out',
-            backgroundImage: `url(${image})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            height: '100%',
-            width: '100%'
-          }}
-        />
+      <Box sx={classes.backgroundImageWrapper}>
+        {backgroundImage ? (
+          <Box className='image' sx={classes.backgroundImage} />
+        ) : (
+          <Box sx={classes.imageWrapper}>
+            <img src={imageUrl} alt={name} />
+          </Box>
+        )}
+        {novelty && (
+          <Chip
+            label='Neu'
+            color='primary'
+            size='small'
+            sx={classes.noveltyLabel}
+          />
+        )}
       </Box>
       <Box sx={{ p: 4 }}>
-        <Typography gutterBottom sx={{ fontSize: 16 }} variant='h6'>
-          {name}
+        <Typography gutterBottom variant='h6' sx={{ minHeight: 52 }}>
+          <TextTruncate line={2} truncateText='…' text={name} />
         </Typography>
+        {tags && (
+          <Box sx={classes.tagsWrapper}>
+            {tags.map((item) => {
+              return <Chip label={item} size='small' sx={{ mr: 2, mb: 2 }} />
+            })}
+          </Box>
+        )}
         <Points points={points} />
       </Box>
     </Box>
