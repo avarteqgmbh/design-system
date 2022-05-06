4de656eeac5db58558ce9b642efc9d52c2737b31
Niklas Kiefer <nkiefer@anynines.com>
2022-05-06 15:33:37 +0200

    Add: SelectableCard CHAP-2073

---
+++ src/templates/cards/SelectableCard/SelectableCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Card, CardContent, Radio, Typography } from '../../../index'
+
+export interface SelectableCardProps {
+  children: React.ReactNode
+  cardID: string
+  title: string
+  options?: React.ReactNode
+  setSelected: (id: string) => void
+  selectedID: string
+}
+
+export const SelectableCard: React.FC<SelectableCardProps> = (props) => {
+  const { children, cardID, title, options, setSelected, selectedID } = props
+
+  return (
+    <Card sx={{ mb: 4 }}>
+      <CardContent
+        sx={{
+          borderBottom: '1px solid',
+          borderBottomColor: 'background.medium',
+          bgcolor: 'background.light',
+          display: 'flex',
+          alignItems: 'center',
+          py: 3,
+          px: 4
+        }}
+      >
+        <Radio
+          checked={selectedID === cardID}
+          onClick={(): void => {
+            setSelected(cardID)
+          }}
+          sx={{ p: 0, mr: 2 }}
+        />
+        <Typography fontWeight={800} sx={{ flex: 1 }}>
+          {title}
+        </Typography>
+        {options && options}
+      </CardContent>
+      <CardContent sx={{ bgcolor: 'background.paper' }}>{children}</CardContent>
+    </Card>
+  )
+}
