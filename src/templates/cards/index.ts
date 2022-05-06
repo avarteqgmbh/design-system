2dbb49d14e16eeb75253aba499ab1ae0399441cc
Niklas Kiefer <nkiefer@anynines.com>
2022-05-06 12:00:15 +0200

    Add: HighlightProductCard CHAP-2073

---
+++ src/templates/cards/index.ts
@@ -0,0 +1,19 @@
+import { BaseCard, CardProps } from './BaseCard/BaseCard'
+import {
+  HighlightProductCard,
+  HighlightProductCardProps
+} from './HighlightProductCard/HighlightProductCard'
+import { ProductCard, ProductCardProps } from './ProductCard/ProductCard'
+import {
+  VerticalProductCard,
+  VerticalProductCardProps
+} from './VerticalProductCard/VerticalProductCard'
+
+export { BaseCard, HighlightProductCard, ProductCard, VerticalProductCard }
+
+export type {
+  CardProps,
+  HighlightProductCardProps,
+  ProductCardProps,
+  VerticalProductCardProps
+}
