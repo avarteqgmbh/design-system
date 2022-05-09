aefbd873eff62b4e8c454c5be29eeaf09c3e1c0c
Niklas Kiefer <nkiefer@anynines.com>
2022-05-09 11:51:05 +0200

    Update: SelectableCard and add borderRadius to components/Card CHAP-2073

--- src/templates/cards/SelectableCard/SelectableCard.tsx
+++ src/templates/cards/SelectableCard/SelectableCard.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import TextTruncate from 'react-text-truncate'
-import { Card, CardContent, Radio, Typography } from '../../../index'
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Radio,
+  Typography
+} from '../../../index'
 
 export interface SelectableCardProps {
   children: React.ReactNode
@@ -14,32 +20,45 @@ export interface SelectableCardProps {
 export const SelectableCard: React.FC<SelectableCardProps> = (props) => {
   const { children, cardID, title, options, setSelected, selectedID } = props
 
+  const classes = {
+    root: {
+      mb: 4,
+      boxShadow: selectedID === cardID ? 6 : 1
+    },
+    cardHeader: {
+      borderBottom: '1px solid',
+      borderBottomColor: 'background.medium',
+      bgcolor: 'background.light',
+      display: 'flex',
+      alignItems: 'center',
+      py: 3,
+      px: 4
+    },
+    cardContent: {
+      bgcolor: 'background.paper'
+    }
+  }
   return (
-    <Card sx={{ mb: 4 }}>
-      <CardContent
-        sx={{
-          borderBottom: '1px solid',
-          borderBottomColor: 'background.medium',
-          bgcolor: 'background.light',
-          display: 'flex',
-          alignItems: 'center',
-          py: 3,
-          px: 4
-        }}
-      >
-        <Radio
-          checked={selectedID === cardID}
-          onClick={(): void => {
-            setSelected(cardID)
-          }}
-          sx={{ p: 0, mr: 2 }}
-        />
-        <Typography fontWeight={800} sx={{ flex: 1 }}>
-          <TextTruncate line={2} truncateText='…' text={title} />
-        </Typography>
-        {options && options}
-      </CardContent>
-      <CardContent sx={{ bgcolor: 'background.paper' }}>{children}</CardContent>
+    <Card sx={classes.root}>
+      <CardHeader
+        sx={classes.cardHeader}
+        avatar={
+          <Radio
+            checked={selectedID === cardID}
+            onClick={(): void => {
+              setSelected(cardID)
+            }}
+            sx={{ p: 0 }}
+          />
+        }
+        action={options && options}
+        title={
+          <Typography fontWeight={800} sx={{ flex: 1 }}>
+            <TextTruncate line={2} truncateText='…' text={title} />
+          </Typography>
+        }
+      />
+      <CardContent sx={classes.cardContent}>{children}</CardContent>
     </Card>
   )
 }
