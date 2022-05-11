795aab94e413e2b189364ae7716f027b9cf05b2a
Niklas Kiefer <nkiefer@anynines.com>
2022-05-11 13:40:07 +0200

    Update: Badge with Size Prop CHAP-2077

--- src/components/dataDisplay/Badge/Badge.tsx
+++ src/components/dataDisplay/Badge/Badge.tsx
@@ -1,20 +1,43 @@
 import React from 'react'
-import { Badge as MuiBadge, BadgeProps } from '@mui/material'
+import { Badge as MuiBadge, BadgeProps as MuiBadgeProps } from '@mui/material'
+
+export interface BadgeProps extends MuiBadgeProps {
+  size?: 'sm' | 'md' | 'lg'
+}
 
 export function Badge(props: BadgeProps): JSX.Element {
-  const { color = 'primary', sx } = props
+  const { color = 'primary', size = 'sm', sx } = props
 
-  return <MuiBadge sx={{ ...sx, ...styles }} color={color} {...props} />
+  return (
+    <MuiBadge
+      className={size}
+      sx={{ ...sx, ...styles }}
+      color={color}
+      {...props}
+    />
+  )
 }
 
 const styles = {
-  '& .MuiBadge-badge': {
-    border: '1px solid',
-    borderColor: 'background.paper'
-  },
   '& .MuiBadge-dot': {
     height: '10px',
     minWidth: '10px',
     borderRadius: '8px'
+  },
+  '&.md': {
+    '& .MuiBadge-dot': {
+      height: 12,
+      minWidth: 12
+    }
+  },
+  '&.lg': {
+    '& .MuiBadge-dot': {
+      height: 16,
+      minWidth: 16
+    }
+  },
+  '& .MuiBadge-badge': {
+    border: '1px solid',
+    borderColor: 'background.paper'
   }
 }
