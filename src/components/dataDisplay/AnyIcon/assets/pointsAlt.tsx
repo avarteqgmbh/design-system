ce4ae1d8d9945f5a8195c2cfee83d67243af9589
Niklas Kiefer <nkiefer@anynines.com>
2022-05-18 11:46:19 +0200

    Remove: dataName from coinStack and pointsAlt CHAP-2149

--- src/components/dataDisplay/AnyIcon/assets/pointsAlt.tsx
+++ src/components/dataDisplay/AnyIcon/assets/pointsAlt.tsx
@@ -8,7 +8,7 @@ const pointsAlt: React.FC = () => {
       height='512'
       viewBox='0 0 24 24'
     >
-      <g id='Layer_70' dataName='Layer 70'>
+      <g id='Layer_70'>
         <circle cx='12' cy='12' r='5' fill='currentColor' />
         <path
           d='m12 2a10 10 0 1 0 10 10 10 10 0 0 0 -10-10zm0 17a7 7 0 1 1 7-7 7 7 0 0 1 -7 7z'
