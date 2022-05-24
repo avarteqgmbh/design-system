aab05c0a9c6e45fe1f2cb15ba6eab48193faa241
Niklas K <48592076+nkiefera9s@users.noreply.github.com>
2022-05-24 11:51:33 +0200

    CHAP-2225 base header mobile (#233)
    * Update: Styling for BaseMobileHeader CHAP-2225
    
    * Adjust: props for MobileHeader CHAP-2225
    
    * Add: fullscreen option, defaultViewport for MobileHeader Stories, adjust IconButton and BaseHeaderMobile CHAP-2225
    
    * add missing dependencies to xgrid React.useeffect CHAP-2225
    
    * Adjustments after CR CHAP-2225
    
    * Adjust: Link to Figma CHAP-2225
    
    * Fix: XGrid RegisterLocalStorageEvents
    
    Co-authored-by: Moritz Ney <mney@anynines.com>

---
+++ src/templates/MobileHeader/MobileHeader.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import { withDesign } from 'storybook-addon-designs'
+import { AnyIcon } from '../../components'
+import { BaseHeaderMobile, BaseHeaderMobileProps } from './BaseHeaderMobile'
+
+export default {
+  title: 'Templates/MobileHeader',
+  component: BaseHeaderMobile,
+  decorators: [withDesign],
+  parameters: {
+    controls: { expanded: true },
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/piIRNwoneeyLTKctvITs6z/%F0%9F%93%B1-Mobile?node-id=2791%3A63792'
+    },
+    fullscreen: true,
+    viewport: {
+      defaultViewport: 'iphonex',
+    },
+  }
+}
+
+const Template: Story<BaseHeaderMobileProps> = (args) => {
+  return <BaseHeaderMobile {...args} />
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+  pageTitle: 'Points',
+  points: '1.200',
+  pointsIcon: <AnyIcon icon='points' size='medium' />
+}
+
+export const WithNotifications = Template.bind({})
+WithNotifications.args = {
+  avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
+  notifications: true,
+  newNotifications: true,
+  pageTitle: 'Points',
+  points: '1.200',
+  pointsIcon: <AnyIcon icon='points' size='medium' />
+}
