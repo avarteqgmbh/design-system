ef968041ee3183537a8fa8ab62c394b3ad4d5885
Niklas Kiefer <nkiefer@anynines.com>
2022-05-24 12:08:25 +0200

    Update: Figma Link CHAP-2228

--- src/templates/MainMenuItem/MainMenuItem.stories.tsx
+++ src/templates/MainMenuItem/MainMenuItem.stories.tsx
@@ -12,7 +12,7 @@ export default {
     controls: { expanded: true },
     design: {
       type: 'figma',
-      url: 'https://www.figma.com/file/piIRNwoneeyLTKctvITs6z/%F0%9F%98%81-Happy?node-id=2797%3A66076'
+      url: 'https://www.figma.com/file/FquPS1rVsEsTOPxR8SCw04/%F0%9F%93%9A-Design-System?node-id=404%3A30059'
     }
   }
 }
