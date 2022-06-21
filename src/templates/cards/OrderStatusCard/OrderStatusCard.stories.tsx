4b82c5b1b3748fbad67970d5568b8dbbedf74f50
Niklas K <48592076+nkiefera9s@users.noreply.github.com>
2022-06-21 16:27:24 +0200

    Add: label props for OrderStatusCard CHAP-2274 (#273)

--- src/templates/cards/OrderStatusCard/OrderStatusCard.stories.tsx
+++ src/templates/cards/OrderStatusCard/OrderStatusCard.stories.tsx
@@ -37,8 +37,12 @@ export const Default = Template.bind({})
 Default.args = {
   orderDate: '12.03.2022',
   orderNumber: 'TK-123104901429',
-  releasedDate: '13.06.2022',
+  releasedDate: {
+    label: 'Versendet am',
+    value: '14.03.2022'
+  },
   orderAddress: {
+    label: 'Versandadresse',
     firstName: 'Max4109',
     lastName: 'Muster4109',
     company: 'toyota de',
@@ -48,15 +52,17 @@ Default.args = {
     email: 'Muster4109@bumg.de',
     phone: '06131 4109'
   },
+  pointsInfoLabel: 'Summe',
   orderItems: [
     {
       title: 'Apple Computer "iMac" (24 Zoll / M1 Chip )',
       image:
         'https://s3-eu-west-1.amazonaws.com/bum-stage/articles/1315051-thumb_medium-BM-018235_1.jpg',
       points: 13500,
-      variant: { label: 'Artikel:', value: 'Space-Gray' },
-      amount: 1,
-      sendAt: '12.03.2022',
+      variant: { label: 'Artikel', value: 'Space-Gray' },
+      amount: { label: 'Anzahl', value: 1 },
+      sendAt: { label: 'Versendet am', value: '14.03.2022' },
+      trackingLabel: 'Lieferung verfolgen',
       onClick: () => {
         return null
       }
@@ -66,8 +72,8 @@ Default.args = {
       backgroundImage: true,
       image:
         'https://images.pexels.com/photos/3270223/pexels-photo-3270223.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      variant: { label: 'Farbe:', value: 'Blau' },
-      amount: 1,
+      variant: { label: 'Farbe', value: 'Blau' },
+      amount: { label: 'Anzahl', value: 1 },
       points: 349
     }
   ]
